c24e87ebec6f065ee28bd575fcc0c08791ce261e
Davo <davidgalavotti@gmail.com>
2012-12-09 20:55:06 -0300

    Cambios en ruteo para apuntar a API

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@
  */
 
 var fs = require('fs');
+var request = require('superagent');
+
 
 /*
 * GET home page.
@@ -17,12 +19,19 @@ exports.acerca = function(req, res){
   res.render('acerca', { title: 'Media Sanción | Acerca' });
 };
 
+// exports.proyectos = function(req, res){
+//   fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
+//     data = JSON.parse(data);
+//     res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
+//   });
+// };
+
 exports.proyectos = function(req, res){
-  fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
-    data = JSON.parse(data);
-    res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
+  request('http://198.74.50.217:8011/api/0/proyectos/572759af-0b40-4f35-ba45-8a80bd9b8ebb/?format=json', function(res){
+  	res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: res.body });
   });
 };
+
 
 exports.legisladores = function(req, res){
   res.render('legisladores', { title: 'Media Sanción | Legisladores' });
