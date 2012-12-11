d818f29d97a3fdf04d345feb52cae192b68f017b
Davo <davidgalavotti@gmail.com>
2012-12-11 08:50:03 -0300

    Cambios en las vistas de Legisladores y Comisiones

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
  * Module dependencies
  */
 
-//var fs = require('fs');
+var fs = require('fs');
 var request = require('superagent');
 var projectList, projectUniqueId;
 
@@ -17,6 +17,11 @@ var projectUniqueId = function(uuid){
   return 'http://198.74.50.217:8011/api/0/proyectos/'+uuid+'/?format=json';
 };
 
+var legislatorsList = 'http://198.74.50.217:8011/api/0/legisladores/?format=json';
+
+var comissionsList = 'http://198.74.50.217:8011/api/0/comisiones/?format=json';
+
+
 /*
 * GET home page.
 */
@@ -29,18 +34,18 @@ exports.acerca = function(req, res){
   res.render('acerca', { title: 'Media Sanción | Acerca' });
 };
 
-// exports.proyectos = function(req, res){
-//   fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
-//     data = JSON.parse(data);
-//     res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
-//   });
-// };
-
 exports.proyectos = function(req, res){
-  request(projectList, function(resp){
-  	res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: resp.body });
+  fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
+    data = JSON.parse(data);
+    res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
   });
 };
+
+// exports.proyectos = function(req, res){
+//   request(projectList, function(resp){
+//   	res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: resp.body });
+//   });
+// };
 
 exports.proyectosPageId = function(req, res){
   request(projectListPage(req.params.page_id), function(resp){
@@ -54,12 +59,14 @@ exports.proyecto = function(req, res){
   });
 };
 
-
 exports.legisladores = function(req, res){
-  res.render('legisladores', { title: 'Media Sanción | Legisladores' });
+  request(legislatorsList, function(resp){
+    res.render('legisladores', { title: 'Media Sanción | Legisladores', legisladores: resp.body });
+  });
 };
 
 exports.comisiones = function(req, res){
-  res.render('comisiones', { title: 'Media Sanción | Comisiones' });
+  request(comissionsList, function(resp){
+    res.render('comsiones', { title: 'Media Sanción | Comisiones', comsiones: resp.body });
+  });
 };
-
