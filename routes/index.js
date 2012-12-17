f54c643498e96f3c3a72d7590c19585faf4448fb
Davo <davidgalavotti@gmail.com>
2012-12-17 02:01:23 -0300

    Iteracion con sumario de cada proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@
  * Module dependencies
  */
 
-var fs = require('fs');
+//var fs = require('fs');
 var request = require('superagent');
 var projectList, projectUniqueId;
 
-var projectList = 'http://198.74.50.217:8011/api/0/proyectos/?format=json';
+var projectList = 'http://198.74.50.217:8011/api/0/proyectos/?fields=sumario,-uuid&format=json';
 
 var projectListPage = function(id){ 
   return 'http://198.74.50.217:8011/api/0/proyectos/?page='+id+'&format=json';
@@ -34,18 +34,18 @@ exports.acerca = function(req, res){
   res.render('acerca', { title: 'Media Sanción | Acerca' });
 };
 
-exports.proyectos = function(req, res){
-  fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
-    data = JSON.parse(data);
-    res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
-  });
-};
-
 // exports.proyectos = function(req, res){
-//   request(projectList, function(resp){
-//   	res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: resp.body });
+//   fs.readFile(__dirname + '/../data/proyecto-list.json', 'utf-8', function(err, data){
+//     data = JSON.parse(data);
+//     res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: data });
 //   });
 // };
+
+exports.proyectos = function(req, res){
+  request(projectList, function(resp){
+  	res.render('proyectos', { title: 'Media Sanción | Proyectos', proyectos: resp.body });
+  });
+};
 
 exports.proyectosPageId = function(req, res){
   request(projectListPage(req.params.page_id), function(resp){
