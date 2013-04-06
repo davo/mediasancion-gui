4976369534dc9e79ebc6371e15b861fb68a0a25b
Dan Zajdband <dan.zajdband@gmail.com>
2013-04-06 17:31:23 -0300

    change search cat

---
+++ public/javascripts/tools.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+	var $searchCategory = $('#searchCategory');
+
+	$('#searchCatList button').click(function(e){
+		var id = $(this).attr('id').split('-');
+		var cat = id[1];
+		
+		$searchCategory.val(cat);
+		e.preventDefault();
+	});
+
+
+
+});
